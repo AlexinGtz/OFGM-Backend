e466d2b384c09352f02ccc62184258f6f857d71d
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-06-26 23:09:29 -0500

    Fixed code smells

--- src/testLambda.ts
+++ src/testLambda.ts
@@ -5,9 +5,6 @@ export const handler = async (event: eventType) => {
         console.log('Has id');
     }
 
-    const res = event.name + 'Hola';
-    const anotherRes = event.age ? event.age * 2 : null;
-
     console.log(process.env.USERS_TABLE);
 
     return {
