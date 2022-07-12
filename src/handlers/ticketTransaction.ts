4f0eee8cb5df303cc0da7c07530e5fe52d95da49
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-07-11 22:34:13 -0500

    Changed source for concertId

--- src/handlers/ticketTransaction.ts
+++ src/handlers/ticketTransaction.ts
@@ -57,7 +57,7 @@ export const handler = async (event: ConcertEventType) => {
 
         const ticketItem = {
             ...ticket,
-            concert: ticket.concert?.concertId,
+            concert: concertId,
             key: s3Params.Key,
             date: new Date().toISOString(),
             scanned: false,
